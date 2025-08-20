a3ba266de4a47e63ee286c4d0f290f20bb1e6550
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2025-08-20 11:30:18 +0000

    adding num_math file

---
+++ basics/num_math.js
@@ -0,0 +1,24 @@
+//  ++++++++++++++++++++ Number +++++++++++++++++++++++
+const number = new Number(300)
+console.log(number)
+console.log(number.toExponential(3));
+console.log(typeof number);
+console.log(number.toString);
+console.log(typeof number)
+
+
+
+// +++++++++++++++++++++++++++ Math +++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.ceil(3.6));
+console.log(Math.floor(5.9))
+console.log(Math.ceil((Math.random()*5)+1))
+console.log(Math.abs(-40));
+
+
+
+
+
+
