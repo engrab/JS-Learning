58c5188581727b87b5330fefa7ea84c6e08f978c
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2025-08-20 05:23:50 +0000

    converation of types

---
+++ basics/converationoperation.js
@@ -0,0 +1,13 @@
+let score = "33"
+let scoreInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof(scoreInNumber));
+
+let myName = "Bilawal"
+let nameNumber = Number(myName)
+console.log(nameNumber) // NaN Not a Number.
+
+let isLoading = true
+let IsLoadingNumber = Number(isLoading)
+console.log(IsLoadingNumber); // true => 1 ; false => 0
+console.log(typeof IsLoadingNumber)
---
+++ basics/variable.js
@@ -0,0 +1,6 @@
+const PI = 3.14
+let name = "Bilawal"
+var age = 30
+city = "khangarh"
+
+console.table([PI, name, age, city])
