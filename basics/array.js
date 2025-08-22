8b5092463eb40374b07b064b68941e6e3a039c54
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2025-08-22 09:59:57 +0000

    adding array

---
+++ basics/array.js
@@ -0,0 +1,17 @@
+const fruite = ["seeb", "angoor", "nashpati"]
+const another_fruit = ["kely", "anar", "japani phal"]
+
+// const all_fruite = fruite.concat(another_fruit)
+// console.log(all_fruite);
+
+// const tamam_fruit = [...fruite, ...another_fruit]
+// console.log(tamam_fruit);
+
+// const push_fruite = fruite.push(another_fruit)
+// console.log(push_fruite);
+
+const myArr = fruite.flat(another_fruit)
+console.log(myArr);
+
+
+
