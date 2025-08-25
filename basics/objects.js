a26497b5e0732a866acab25a9796e8885d467399
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2025-08-25 01:32:11 +0000

    destructure of object

--- basics/objects.js
+++ basics/objects.js
@@ -1,4 +1,7 @@
-// This is symbol 
+// singleton object
+const singletonObject = new Object()
+
+// This is lieteral object 
 const mySym = Symbol("Developer")
 // This is object
 const jsUser = {
@@ -14,5 +17,30 @@ console.log(jsUser["name"]);
 console.log(jsUser.email);
 console.log(jsUser[mySym])
 console.log(typeof jsUser[mySym])
+
+// nested object
+const nestObject = {
+    username: {
+        fullname:{
+            firstname: "Bilawal",
+            lastname: "Hussain"
+        }
+    }
+}
+console.log(nestObject.username.fullname.firstname);
+
+const joinObject = {...jsUser, ...nestObject}
+console.log(joinObject);
+console.log(joinObject.username.fullname.lastname);
+
+// de structure of object
+
+const {name: n} = jsUser
+console.log(n);
+console.log(n);
+console.log(n);
+console.log(n);
+
+
 
 
