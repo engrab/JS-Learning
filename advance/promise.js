e5d25cd5b059b8b9b03a89c179fcb3785ffb0d63
Engr Bilawal Hussain <35764126+engrab@users.noreply.github.com>
2025-09-14 12:16:40 +0000

    promise adding

---
+++ advance/promise.js
@@ -0,0 +1,53 @@
+const promiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("promise one executed")
+        resolve()
+    }, 1000);
+})
+
+promiseOne.then()
+
+new Promise((resolve, reject) => {
+    setInterval(() => {
+        console.log("promoise two executed")
+        resolve()
+    }, 1000);
+}).then((resolve) => {
+    console.log(resolve)
+})
+
+const promiseThree = new Promise((resolve, reject) => {
+    let error = true;
+    if(!error){
+        resolve("this is resolve three")
+    }else{
+        reject("some thing went wrong")
+    }
+})
+promiseThree.then((data) => {
+    console.log(data)
+}).catch((error) => {
+    console.log(error)
+})
+
+const promiseFour = new Promise((resolve, reject) => {
+    setTimeout( () => {
+        console.log("This is Four prmoise")
+    }, 1000)
+})
+
+
+async function consumePromiseFour() {
+    try {
+        const cnsPromiseFour = await promiseFour
+        console.log(cnsPromiseFour)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+consumePromiseFour()
